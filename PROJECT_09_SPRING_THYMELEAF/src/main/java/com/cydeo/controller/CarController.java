31e199c0dbf4cdfcf7c6c36561b941430782fbf6
olegtsetnar <oleg.cetnar@gmail.com>
2023-01-05 11:15:18 -0800

    commit MVC9

--- PROJECT_09_SPRING_THYMELEAF/src/main/java/com/cydeo/controller/CarController.java
+++ PROJECT_09_SPRING_THYMELEAF/src/main/java/com/cydeo/controller/CarController.java
@@ -3,18 +3,37 @@ package com.cydeo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/car")
 public class CarController {
 
-@RequestMapping("/info")
-    public String carInfo (Model model){
 
+    @RequestMapping("/info")
+    /* when I request localhost:8080/car/info?make=honda
+    I want to print honda
+    */
+    public String carInfo(@RequestParam String make,@RequestParam Integer year, Model model) {
+        System.out.println(make);
 
+/*
+ then we want to paste this make- honda into html
+ */
+        model.addAttribute("make", make);
+        model.addAttribute("year",year);
+        return "car/info";
+    }
 
+    @RequestMapping("/info2")
+    public String carInfo2
+             /* when I request localhost:8080/car/info2?make=honda -->honda
+     when I request localhost:8080/car/info2 -->tesla
+    */
+            (@RequestParam(value = "make", defaultValue = "Tesla") String make, Model model) {
+        System.out.println(make);
 
-
+        model.addAttribute("make", make);
 
         return "car/info";
     }
